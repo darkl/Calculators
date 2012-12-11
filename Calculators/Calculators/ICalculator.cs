ed588d13d93bf161af532a06834ebdab330261c6
Amit Yogev <amit.yogev90@gmail.com>
2012-12-11 20:35:37 +0200

    adding new ICalculator interface

---
+++ Calculators/Calculators/ICalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculators
+{
+    public interface ICalculator
+    {
+        TResult Add<T1, T2, TResult>(T1 first, T2 second);
+        TResult Subtract<T1, T2, TResult>(T1 first, T2 second);
+        TResult Multiply<T1, T2, TResult>(T1 first, T2 second);
+        TResult Divide<T1, T2, TResult>(T1 first, T2 second);
+    }
+}
