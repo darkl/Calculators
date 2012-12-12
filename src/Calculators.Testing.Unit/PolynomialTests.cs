dd0d7b0231d54c7f68403e81d0c56bd8a4c8a53f
darkl <dark_leizar@walla.com>
2012-12-12 23:41:37 +0200

    Implemented polynomial division

---
+++ src/Calculators.Testing.Unit/PolynomialTests.cs
@@ -0,0 +1,25 @@
+﻿using Calculators.Algebra;
+using NUnit.Framework;
+
+namespace Calculators.Testing.Unit
+{
+    [TestFixture]
+    public class PolynomialTests
+    {
+        [Test]
+        public void TestDivision()
+        {
+            PolynomialOverFieldRing<double> ring = new PolynomialOverFieldRing<double>(new SlowOperatorBasedField<double>());
+
+            Polynomial<double> remainder;
+
+            var quotient =
+                ring.Divide(new Polynomial<double>(new double[] { -1, 0, 0, 1 }, ring.CoefficientsRing),
+                            new Polynomial<double>(new double[] { -1, 1 }, ring.CoefficientsRing), out remainder);
+
+            Assert.IsTrue(ring.Comparer.Equals(remainder, ring.Zero));
+
+            Assert.IsTrue(ring.Comparer.Equals(quotient, new Polynomial<double>(new double[] {1, 1, 1}, ring.CoefficientsRing)));
+        }
+    }
+}
---
+++ src/Calculators/Algebra/Implementation/PolynomialExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using Calculators.Algebra.Abstract;
+
+namespace Calculators.Algebra
+{
+    public static class PolynomialExtensions
+    {
+        public static Polynomial<T> CreateMonomial<T>(this IRing<T> coefficientsRing,
+                                                   T leadingCoefficient,
+                                                   int degree)
+        {
+            T[] coefficients = Enumerable.Repeat(coefficientsRing.Zero, degree + 1).ToArray();
+            coefficients[degree] = leadingCoefficient;
+            return new Polynomial<T>(coefficients, coefficientsRing);
+        }
+
+        public static T LeadingCoefficient<T>(this Polynomial<T> polynomial)
+        {
+            return polynomial[polynomial.Degree];
+        }
+    }
+}
