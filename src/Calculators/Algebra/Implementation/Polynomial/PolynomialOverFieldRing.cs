0062a4556e6178070b82da43f48f671972bdbb4f
darkl <dark_leizar@walla.com>
2012-12-14 00:51:19 +0200

    Added a cool polynomial parser

---
+++ src/Calculators/Algebra/Implementation/Polynomial/PolynomialOverFieldRing.cs
@@ -0,0 +1,44 @@
+﻿using Calculators.Algebra.Abstract;
+
+namespace Calculators.Algebra
+{
+    public class PolynomialOverFieldRing<T> : PolynomialRing<T>, IEuclideanRing<Polynomial<T>>
+    {
+        private readonly IField<T> mCoefficientsField;
+
+        public PolynomialOverFieldRing(IField<T> coefficientsField) : base(coefficientsField)
+        {
+            mCoefficientsField = coefficientsField;
+        }
+
+        public Polynomial<T> Divide(Polynomial<T> dividend, Polynomial<T> divisor, out Polynomial<T> remainder)
+        {
+            Polynomial<T> quotient = this.Zero;
+
+            remainder = dividend;
+
+            T divisorLeadingCoefficient = divisor.LeadingCoefficient();
+
+            while (divisor.Degree <= remainder.Degree)
+            {
+                int degree = remainder.Degree - divisor.Degree;
+                
+                Polynomial<T> currentPart =
+                    mCoefficientsField.CreateMonomial(
+                        mCoefficientsField.Multiply(remainder.LeadingCoefficient(),
+                                                    mCoefficientsField.Inverse(divisorLeadingCoefficient)),
+                        degree);
+
+                remainder = this.Add(remainder, this.Negative(this.Multiply(currentPart, divisor)));
+                quotient = this.Add(quotient, currentPart);
+            }
+
+            return quotient;
+        }
+
+        public int Degree(Polynomial<T> element)
+        {
+            return element.Degree;
+        }
+    }
+}
---
+++ src/Calculators/Algebra/Implementation/Polynomial/PolynomialParser.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Calculators.Algebra.Abstract;
+using Mono.CSharp;
+
+namespace Calculators.Algebra
+{
+    public class PolynomialParser
+    {
+        private readonly Evaluator mEvaluator;
+
+        public PolynomialParser()
+        {
+            mEvaluator = new Evaluator(new CompilerSettings(){AssemblyReferences = 
+                new List<string>(){"System.Core"}}, 
+                new Report(new StreamReportPrinter(new StringWriter(new StringBuilder()))));
+
+            mEvaluator.ReferenceAssembly(typeof(PolynomialOperatorSyntax<>).Assembly);
+
+            mEvaluator.Run("using System;");
+            mEvaluator.Run("using System.Linq.Expressions;");
+            mEvaluator.Run("using Calculators.Algebra;");
+        }
+
+        public Polynomial<T> Parse<T>(string expression, IRing<T> ring)
+        {
+            string declarationFormat = 
+                "Func<PolynomialOperatorSyntax<{0}>, PolynomialOperatorSyntax<{0}>> evaluator = x => ({1});";
+
+            string declaration =
+                string.Format(declarationFormat, typeof (T).FullName, expression);
+
+            mEvaluator.Run(declaration);
+            Func<PolynomialOperatorSyntax<T>, PolynomialOperatorSyntax<T>> evaluator =
+                mEvaluator.Evaluate("evaluator;")
+                as Func<PolynomialOperatorSyntax<T>, PolynomialOperatorSyntax<T>>;
+
+            PolynomialRing<T> polynomialRing = new PolynomialRing<T>(ring);
+            
+            PolynomialOperatorSyntax<T> evaluated =
+                evaluator(new PolynomialOperatorSyntax<T>(ring.CreateMonomial(ring.Identity, 1), polynomialRing));
+
+            return evaluated.Value;
+        }
+    }
+}
